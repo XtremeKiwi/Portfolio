874820f100e92c831ecd72cdd5689f708f902d70
XtremeKiwi <kiwimai96@gmail.com>
2025-07-26 12:01:39 +0200

    Added SoMe section

---
+++ JS/app.js
@@ -0,0 +1,18 @@
+// mediaquery with javascript //
+
+const x = window.matchMedia("(max-width: 340px)")
+
+function mediaqueryHeaderName() {
+    if (x.matches){
+        document.querySelector(".header-name").innerHTML = "KIWI-MAI"
+    } else {
+        document.querySelector(".header-name").innerHTML = "{ KIWI-MAI }"
+
+    }
+}
+
+mediaqueryHeaderName()
+
+x.addEventListener("change", function(){
+    mediaqueryHeaderName();
+});
