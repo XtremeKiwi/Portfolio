c25c20e0e1984feec15e06463e2caef314a3610f
XtremeKiwi <kiwimai96@gmail.com>
2025-07-28 01:51:15 +0200

    Added portfolioData.js and workExperienceData.js

---
+++ JS/data/portfolioData.js
@@ -0,0 +1,12 @@
+export const portfolioPiece = [{
+    name: "Dypshoria Designs",
+    img: "img/Test-image.png",
+    alt: "Dysphoria design logo",
+    href: "#"
+    },
+    {
+    name: "Dypshoria Designs",
+    img: "img/Test-image.png",
+    alt: "Dysphoria design logo",
+    href: "#"
+    }]
