98a2fda790c3b7b72bc4eebecaecc2853d4ef70a
XtremeKiwi <kiwimai96@gmail.com>
2025-07-28 01:42:41 +0200

    Added mediaQuery.js

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,3 @@
-// mediaquery with javascript //
+import {mediaQuery} from "./mediaQuery.js";
 
-const x = window.matchMedia("(max-width: 340px)")
-
-function mediaqueryHeaderName() {
-    if (x.matches){
-        document.querySelector(".header-name").innerHTML = "KIWI-MAI"
-    } else {
-        document.querySelector(".header-name").innerHTML = "{ KIWI-MAI }"
-
-    }
-}
-
-mediaqueryHeaderName()
-
-x.addEventListener("change", function(){
-    mediaqueryHeaderName();
-});
+mediaQuery();
---
+++ JS/mediaQuery.js
@@ -0,0 +1,17 @@
+export function mediaQuery(){
+const x = window.matchMedia("(max-width: 340px)")
+
+function mediaqueryHeaderName() {
+    if (x.matches){
+        document.querySelector(".header-name").innerHTML = "KIWI-MAI"
+    } else {
+        document.querySelector(".header-name").innerHTML = "{ KIWI-MAI }"
+
+    }
+}
+
+mediaqueryHeaderName()
+
+x.addEventListener("change", function(){
+    mediaqueryHeaderName();
+})}
