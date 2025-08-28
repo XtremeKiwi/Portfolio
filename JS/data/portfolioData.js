35d47f3ea4eb01fa2beb0d345ee319d9f2d2c42d
XtremeKiwi <kiwimai96@gmail.com>
2025-08-28 22:16:10 +0200

    Added portfolio template and a few portfolio pieces, as well as directories for pictures

--- JS/data/portfolioData.js
+++ JS/data/portfolioData.js
@@ -3,15 +3,33 @@ const portfolioPiece1 = [{
         name: 'Juncle (eksamen)',
         img: 'img/Jungle logo.png',
         alt: 'Dysphoria design logo',
-        href: 'https://mastodon.social/home'
+        href: 'portfolioPiecesDir/portfolioPieceTemplate.html'
     }]
 
 // 3D //
 const portfolioPiece2 = [{
     name: 'Svampen Bob',
-    img: 'img/Svampebob_render_final_1.png',
-    alt: 'Dysphoria design logo',
-    href: '#'
+    img: 'img/portfolioImgs/svampenBob/Svampebob_render_final_1.webp',
+    alt: 'Svampen Bob',
+    href: 'portfolioPiecesDir/svampenBob.html'
+    },
+    {
+    name: 'Nerf This!',
+    img: 'img/portfolioImgs/nerfThis/High render 4.jpg',
+    alt: 'Nerf this!',
+    href: 'portfolioPiecesDir/nerfThis.html'
+    },
+    {
+    name: 'Demon Chicken',
+    img: 'img/portfolioImgs/demonChicken/Render test 6.jpg',
+    alt: 'Demon Chicken',
+    href: 'portfolioPiecesDir/demonChicken.html'
+    },
+    {
+    name: 'Beholder',
+    img: '/img/portfolioImgs/beholder/Render test 2.webp',
+    alt: 'Beholder',
+    href: 'portfolioPiecesDir/beholder.html'
 }]
 
 //Fysiske produkter//
@@ -20,18 +38,18 @@ const portfolioPiece3 = [{
     img: 'img/Admech.webp',
     alt: 'Dysphoria design logo',
     href: '#'
-},
+    },
     {
-        name: 'T-shirt',
-        img: 'img/Admech.webp',
-        alt: 'Dysphoria design logo',
-        href: '#'
+    name: 'T-shirt',
+    img: 'img/Admech.webp',
+    alt: 'Dysphoria design logo',
+    href: '#'
     }]
 
 //Concept art
 const portfolioPiece4 = [{
     name: 'Dysphoria Miniatures',
     img: 'img/Daemon Princess of Slaanesh.jpg',
-    alt: 'Dysphoria design logo',
-    href: '#'
+    alt: 'Dysphoria design miniatures',
+    href: 'portfolioPiecesDir/dysphoriaMiniatures.html'
 }]
